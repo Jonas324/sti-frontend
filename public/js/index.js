aa85b551cc9c6805fc7902147b2b35ce65b61b91
Jonas324 <jonaswadin24@gmail.com>
2022-02-17 10:02:08 +0100

    hejsan

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,34 @@
-function addRow() {
-    let table = document.getElementById("myTable");
-  
-    let row = table.insertRow(0);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    cell1.innerHTML = "cell 1";
-    cell2.innerHTML = "cell 2";
+function age(){
+  var xhr = new XMLHttpRequest()
+  xhr.open("GET", "/js/data.json")
+  xhr.onload = function(){
+    var data = JSON.parse(this.response)
+    createTable(data)
   }
-  
-  function deleteRow() {
-    let table = document.getElementById("myTable");
-    table.deleteRow(0);
-  }
+  xhr.send()
+}
+
+function createTable(data){
+  var appElemment = document.getElementById("app")
+  var aTable =document.createElement("table")
+  appElemment.appendChild(aTable)
+  aTable.appendChild(createRow(data[0].name, data[0].points))
+  aTable.appendChild(createRow(data[1].name, data[1].points))
+  aTable.appendChild(createRow(data[2].name, data[2].points))
+  aTable.appendChild(createRow(data[3].name, data[3].points))
+}
+
+function createRow(name, points){
+  var aRow = document.createElement("tr")
+  aRow.appendChild(createCell(name))
+  aRow.appendChild(createCell(points))
+  return aRow
+}
+
+function createCell(content){
+  var aCell = document.createElement("td")
+  aCell.innerHTML = content
+  return aCell
+}
+
+age()
